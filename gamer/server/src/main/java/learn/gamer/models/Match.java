b411a71c75de78443959e5852b65b16c36ce4e45
jwuwj <jwu5work@gmail.com>
2023-06-27 11:38:15 -0400

    Completed Game, Match, and Gender objects in models package

--- gamer/server/src/main/java/learn/gamer/models/Match.java
+++ gamer/server/src/main/java/learn/gamer/models/Match.java
@@ -1,4 +1,17 @@
 package learn.gamer.models;
 
+import java.time.LocalDateTime;
+
 public class Match {
+    private int matchId;
+    private AppUser appUser1;
+    private AppUser appUser2;
+    private LocalDateTime matchTime;
+
+    public Match(int matchId, AppUser appUser1, AppUser appUser2, LocalDateTime matchTime) {
+        this.matchId = matchId;
+        this.appUser1 = appUser1;
+        this.appUser2 = appUser2;
+        this.matchTime = matchTime;
+    }
 }
