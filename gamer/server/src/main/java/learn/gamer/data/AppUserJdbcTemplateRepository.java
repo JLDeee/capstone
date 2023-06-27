1a59767539de9cf7422055208c94829725cd71aa
JLDeee <jackieluu00@gmail.com>
2023-06-27 14:35:59 -0400

    did work in appUserMapper

--- gamer/server/src/main/java/learn/gamer/data/AppUserJdbcTemplateRepository.java
+++ gamer/server/src/main/java/learn/gamer/data/AppUserJdbcTemplateRepository.java
@@ -1,4 +1,23 @@
 package learn.gamer.data;
 
+import learn.gamer.models.AppUser;
+
 public class AppUserJdbcTemplateRepository {
+
+    private final RowMapper<SolarPanel> mapper = (resultSet, rowIndex) -> {
+        AppUser appUser  = new AppUser();
+
+        solarPanel.setId(resultSet.getInt("id"));
+        solarPanel.setSection(resultSet.getString("section"));
+        solarPanel.setRow(resultSet.getInt("row"));
+        solarPanel.setColumn(resultSet.getInt("column"));
+        solarPanel.setYearInstalled(resultSet.getInt("year_installed"));
+
+        Material material = Material.valueOf(resultSet.getString("material"));
+        solarPanel.setMaterial(material);
+
+        solarPanel.setTracking(resultSet.getBoolean("is_tracking"));
+
+        return solarPanel;
+    };
 }
--- gamer/server/src/main/java/learn/gamer/data/mappers/AppUserMapper.java
+++ gamer/server/src/main/java/learn/gamer/data/mappers/AppUserMapper.java
@@ -1,4 +1,31 @@
 package learn.gamer.data.mappers;
 
-public class AppUserMapper {
+import learn.gamer.models.AppUser;
+import learn.gamer.models.Gender;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class AppUserMapper implements RowMapper<AppUser> {
+    private final List<String> roles;
+
+    public AppUserMapper(List<String> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public AppUser mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new AppUser(
+                rs.getInt("app_user_id"),
+                rs.getString("email"),
+                rs.getString("`password`"),
+                rs.getString("gamer_tag"),
+                rs.getDate("birth_date").toLocalDate(),
+                rs.getString("bio"),
+                rs.getBoolean("enabled"),
+                Gender.valueOf(rs.getString("gender_type")),
+                roles);
+    }
 }
