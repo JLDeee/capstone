6e70eb3337ed2952fce9f107171a346ae6d00ddb
JLDeee <jackieluu00@gmail.com>
2023-06-27 12:33:36 -0400

    finished dataAccessException, globalExceptionHandler, and errorResponse classes

--- gamer/server/src/main/java/learn/gamer/controllers/GlobalExceptionHandler.java
+++ gamer/server/src/main/java/learn/gamer/controllers/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package learn.gamer.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    // "Catch all" handler
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+
+        return new ResponseEntity<>(
+                new ErrorResponse("Sorry, but our princess is in another castle..."),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
