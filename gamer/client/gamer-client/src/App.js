8fb2fe177f924951505cb9bcd7537a51e8c5f80f
jwuwj <jwu5work@gmail.com>
2023-06-30 10:33:07 -0400

    Adding Login and SignUp (incomplete) as well as updated App with proper imports

--- gamer/client/gamer-client/src/App.js
+++ gamer/client/gamer-client/src/App.js
@@ -1,9 +1,66 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Login from "./components/Login";
+import AuthContext from "./context/AuthContext";
+import jwtDecode from "jwt-decode";
+import SignUp from "./components/SignUp";
+
+const LOCAL_STORAGE_TOKEN_KEY = "gamersGuildToken";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const[restoreLoginAttemptCompleted, setRestoreLoginAttemptCompleted] = useState(false);
+
+  useEffect( () => {
+    const token = localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY);
+    if(token) {
+      Login(token);
+    }
+    setRestoreLoginAttemptCompleted(true);
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY, token);
+    const { sub: username, authorities: authoritiesString } = jwtDecode(token);
+    const roles = authoritiesString.split(",");
+    const user = {
+      username,
+      roles,
+      token,
+      hasRole(role) {
+        return this.roles.includes(role);
+      }
+    };
+    setUser(user);
+    return user;
+  }
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem(LOCAL_STORAGE_TOKEN_KEY);
+  }
+
+  const auth = {
+    user: user ? {...user} : null,
+    login,
+    logout
+  }
+
+  if (!restoreLoginAttemptCompleted) {
+    return null;
+  }
+
   return (
- <>
- 
- </>
+    <AuthContext.Provider value={auth}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/sign_up" element={<SignUp/>}/>
+          {/* insert other routes here! */}
+        </Routes>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
