36cc82e2e1af485b5dd48fd6c2c50781a96bb276
JLDeee <jackieluu00@gmail.com>
2023-06-30 11:36:07 -0400

    did work in Home component

--- gamer/client/gamer-client/src/components/Home.js
+++ gamer/client/gamer-client/src/components/Home.js
@@ -7,15 +7,46 @@ function Home(){
         </div>
        </header>
        <section>
+
+       <div className="logo">
+       <img src="https://ov10-engine.flamingtext.com/netfu/tmp28000/coollogo_com-152464151.png"></img>
+       </div>
+
+       <div className="splashImage">
+       <img src="https://img.freepik.com/premium-vector/neon-game-joystick-icon-glowing-joystick-brick-wall-background-vector-illustration_175392-111.jpg"></img>
+       </div>
+
+        <div className="getStarted">
+        <Link to={'/'}>Get Started!</Link>
+        </div>
+
        <div className="ourStory">
         <p>We created Gamers' Guild in 2023 with the goal of creating and fun and safe environment,
-             where gamers from all over the world can connect with one another to play games, make friends, and just have a good time while building a community!</p>
+             where gamers from all over the world can connect with one another to play games, make friends, and just have a good time while building a community!
+             from popular games like Pokemon, Call of Duty and League of Legends, to more niche titles like Animal Crossing, Stardew Valley and Dark Souls, 
+             you're sure to find people who share the same interests as you here!</p>
        </div>
 
        <div className="successStory1">
-        <p><i>I found it really hard to find people who shared the same interests as me, 
-            but after finding Gamers' Guild I was able to meet many people who liked the same games I did!
-            Some of my best memories were made on Gamers' Guild. - WomboCombo</i></p>
+        <p><i>I really love Animal Crossing! Since I started using Gamers' Guild, 
+            I've been able to meet a lot of cool people and visit so many fun and interesting villages! 
+            I would definitely recommend this site if you're looking for people who like the same games you do! - Isabelle</i></p>
+       </div>
+
+       <div className="successStory2">
+        <p><i>This site is pretty cool. I play a lot of niche games so it was pretty hard to find anyone to talk to about these games, 
+            but Gamers' Guild made it really easy for me to find people who liked the same niche games I did 
+            and I've been able to make a lot of great friendships on this site! - DanTheMan</i></p>
+       </div>
+
+       <div className="successStory3">
+        <p><i>I was suffering in bronze rank for the longest time, 
+            but with the duo finder I was able to find a gaming partner for League of Legends to boost me to diamond. 
+            I'm finally in the rank I deserve!!! - JLDee</i></p>
+       </div>
+
+       <div className="successStory4">
+        <p><i>RAAAAHHHHH! I LOOOVVEEEE THIS SITE!!! RRRRRAAAAAHHHHHH!!! - RagingTeemo</i></p>
        </div>
 
        </section>
