a91bdd6317b51f851685d27b4502bc78e459a324
JLDeee <jackieluu00@gmail.com>
2023-06-30 10:37:23 -0400

    did work in components folder. Duos, Home, Navbar, and Contact

---
+++ gamer/client/gamer-client/src/components/Home.js
@@ -0,0 +1,20 @@
+function Home(){
+    return(<>
+    <div className="container">
+       <header>
+        <div>
+            <h1>Welcome to Gamers' Guild!</h1>
+        </div>
+       </header>
+       <section>
+       <button type="button" id="myBtn" onclick="myFunction()">Try it</button>
+       </section>
+       <footer>
+        <p>Copyright 2023</p>
+       </footer>
+    </div>
+    </>)
+    
+}
+
+export default Home;
