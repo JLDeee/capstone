16df2bf6a87ecc534aa6ec8925e1c887eb8e3bf3
JLDeee <jackieluu00@gmail.com>
2023-06-30 10:51:04 -0400

    did work in home.js

--- gamer/client/gamer-client/src/components/Home.js
+++ gamer/client/gamer-client/src/components/Home.js
@@ -7,7 +7,17 @@ function Home(){
         </div>
        </header>
        <section>
-       <button type="button" id="myBtn" onclick="myFunction()">Try it</button>
+       <div className="ourStory">
+        <p>We created Gamers' Guild in 2023 with the goal of creating and fun and safe environment,
+             where gamers from all over the world can connect with one another to play games, make friends, and just have a good time while building a community!</p>
+       </div>
+
+       <div className="successStory1">
+        <p><i>I found it really hard to find people who shared the same interests as me, 
+            but after finding Gamers' Guild I was able to meet many people who liked the same games I did!
+            Some of my best memories were made on Gamers' Guild. - WomboCombo</i></p>
+       </div>
+
        </section>
        <footer>
         <p>Copyright 2023</p>
